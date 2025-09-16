bad1bf7ebf5375b6cc642cc22c3b0e895ea600a8
MortenHarding <morten.harding@icloud.com>
2025-09-16 14:00:59 +0200

    Changed hostname to hostname.domain.net

--- buildContainer.sh
+++ buildContainer.sh
@@ -8,7 +8,7 @@
 docker build -t mhardingdk/3270bbs:latest .
 
 # Start a container on the new image to verify it's working
-docker run -it --rm --name 3270BBS -h 3270bbs.harding.dk -e TZ=Europe/Copenhagen -v ./data:/opt/3270bbs/data -v ./log:/var/log -p 2022:2022 -p 9000:9000 -p 3270:3270 -p 3271:3271 -p 4443:443 mhardingdk/3270bbs:latest
+docker run -it --rm --name 3270BBS -h hostname.domain.net -e TZ=Europe/Copenhagen -v ./data:/opt/3270bbs/data -v ./log:/var/log -p 2022:2022 -p 9000:9000 -p 3270:3270 -p 3271:3271 -p 4443:443 mhardingdk/3270bbs:latest
 
 # Push new docker image to hub.docker.com. Change to your own repo in Docker hub, before running this line.
 #docker push mhardingdk/3270bbs:latest
@@ -18,4 +18,4 @@ docker run -it --rm --name 3270BBS -h 3270bbs.harding.dk -e TZ=Europe/Copenhagen
 #docker system prune -af
 
 # Test that the new image can be loaded from hup.docker.com. Only needed when refreshing the container image in mhardingdk/3270bbs:latest
-#docker run -it --rm --name 3270BBS -h 3270bbs.harding.dk -e TZ=Europe/Copenhagen -v ./data:/opt/3270bbs/data -v ./log:/var/log -p 2022:2022 -p 9000:9000 -p 3270:3270 -p 3271:3271 -p 4443:443 mhardingdk/3270bbs:latest
+#docker run -it --rm --name 3270BBS -h hostname.domain.net -e TZ=Europe/Copenhagen -v ./data:/opt/3270bbs/data -v ./log:/var/log -p 2022:2022 -p 9000:9000 -p 3270:3270 -p 3271:3271 -p 4443:443 mhardingdk/3270bbs:latest
