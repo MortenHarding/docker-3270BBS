1446207302268a72ca79c302bcf8988756a55109
MortenHarding <morten.harding@icloud.com>
2025-12-30 10:23:27 +0100

    Handle more than 1 'latest version' of 3270BBS

--- getTSU.sh
+++ getTSU.sh
@@ -7,10 +7,14 @@ fi
 
 #Find latest release
 ASSETNAME=$(curl --silent -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "https://api.github.com/repos/moshix/3270BBS/releases/latest" | jq -r '.assets[] | {name} | select(.name | contains("'$PLATFORM'")) | .[]')
+for ASSET in ${ASSETNAME}; do
+    LATESTASSET=${ASSET};
+done
+echo $LATESTASSET
 
 #Download latest release
-echo "wget -O /opt/3270bbs/tsu https://github.com/moshix/3270BBS/releases/latest/download/${ASSETNAME}"
-wget -q -O /opt/3270bbs/tsu "https://github.com/moshix/3270BBS/releases/latest/download/${ASSETNAME}"
+echo "wget -O /opt/3270bbs/tsu https://github.com/moshix/3270BBS/releases/latest/download/${LATESTASSET}"
+wget -q -O /opt/3270bbs/tsu "https://github.com/moshix/3270BBS/releases/latest/download/${LATESTASSET}"
 chmod +x /opt/3270bbs/tsu
 
 #Create 3270BBS link if it doesn't exist
